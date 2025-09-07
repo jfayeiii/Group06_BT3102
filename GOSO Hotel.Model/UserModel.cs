9a1b32244d084b8827b5f669d1ec37662858afaf
jfayeiii <aranellajfaye07@gmail.com>
2025-09-07 20:40:59 +0800

    User model class

--- GOSO Hotel.Model/UserModel.cs
+++ GOSO Hotel.Model/UserModel.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace GOSO_Hotel.Model
 {
-    public class Class1
+    public class UserModel
     {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
